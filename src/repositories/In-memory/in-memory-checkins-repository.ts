bfdbd69a5563992ee6e6c99a1c907d89be72d5e5
Felipe Makoto Gushiken Yui <felipe_m87@yahoo.com.br>
2023-04-15 17:46:43 +0930

    validating date checkin

--- src/repositories/In-memory/in-memory-checkins-repository.ts
+++ src/repositories/In-memory/in-memory-checkins-repository.ts
@@ -2,18 +2,25 @@ import { Prisma, CheckIn } from "@prisma/client";
 
 import { CheckInsRepository } from "../checkins-repository";
 import { randomUUID } from "node:crypto";
+import dayjs from "dayjs";
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = [];
 
-  findByUserIdOnDate(userId: string, date: Date) {
-    const checkInOnTheSameDate = this.items.find(
-      (checkIn) => checkIn.user_id === userId
-    );
-    if (!checkInOnTheSameDate) {
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const startOfTheDay = dayjs(date).startOf("date");
+    const endOfTheDay = dayjs(date).endOf("date");
+
+    const checkInOnSameDate = this.items.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at);
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay);
+      return checkIn.user_id === userId && isOnSameDate;
+    });
+    if (!checkInOnSameDate) {
       return null;
     }
-    return checkInOnTheSameDate;
+    return checkInOnSameDate;
   }
 
   async create(data: Prisma.CheckInUncheckedCreateInput) {
--- src/use-cases/check-ins.spec.ts
+++ src/use-cases/check-ins.spec.ts
@@ -2,15 +2,30 @@ import { it, describe, expect, beforeEach, vi, afterEach } from "vitest";
 
 import { InMemoryCheckInsRepository } from "@/repositories/In-memory/in-memory-checkins-repository";
 import { CheckInUseCase } from "./check-in";
+import { InMemoryGymsRepository } from "@/repositories/In-memory/in-memory-gyms-repository";
+import { Decimal } from "@prisma/client/runtime/library";
 
 let inMemoryCheckInsRepository: InMemoryCheckInsRepository;
+let inMemoryGymsRepository: InMemoryGymsRepository;
 let sut: CheckInUseCase;
 
 describe("Check in Use Case", () => {
   beforeEach(() => {
+    inMemoryGymsRepository = new InMemoryGymsRepository();
     inMemoryCheckInsRepository = new InMemoryCheckInsRepository();
-    sut = new CheckInUseCase(inMemoryCheckInsRepository);
+    sut = new CheckInUseCase(
+      inMemoryCheckInsRepository,
+      inMemoryGymsRepository
+    );
 
+    inMemoryGymsRepository.items.push({
+      id: "gym-01",
+      description: "",
+      title: "JS Gym",
+      phone: "",
+      latitude: new Decimal(0),
+      longitude: new Decimal(0),
+    });
     vi.useFakeTimers();
   });
 
@@ -21,6 +36,8 @@ describe("Check in Use Case", () => {
     const { checkIn } = await sut.execute({
       userId: "user-01",
       gymId: "gym-01",
+      userLatitude: -34.9686588,
+      userLongitude: 138.5687609,
     });
 
     expect(checkIn.id).toEqual(expect.any(String));
@@ -30,6 +47,8 @@ describe("Check in Use Case", () => {
     await sut.execute({
       userId: "user-01",
       gymId: "gym-01",
+      userLatitude: -34.9686588,
+      userLongitude: 138.5687609,
     });
 
     expect(
@@ -37,6 +56,8 @@ describe("Check in Use Case", () => {
         await sut.execute({
           userId: "user-01",
           gymId: "gym-01",
+          userLatitude: -34.9686588,
+          userLongitude: 138.5687609,
         })
     ).rejects.toBeInstanceOf(Error);
   });
@@ -45,11 +66,15 @@ describe("Check in Use Case", () => {
     await sut.execute({
       userId: "user-01",
       gymId: "gym-01",
+      userLatitude: -34.9686588,
+      userLongitude: 138.5687609,
     });
     vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0));
     const { checkIn } = await sut.execute({
       userId: "user-01",
       gymId: "gym-01",
+      userLatitude: -34.9686588,
+      userLongitude: 138.5687609,
     });
 
     expect(checkIn.id).toEqual(expect.any(String));
