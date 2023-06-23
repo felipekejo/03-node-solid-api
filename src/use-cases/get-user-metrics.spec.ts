1fb56c427b005832a33b414b4b7c53d867e25953
Felipe Makoto Gushiken Yui <felipe_m87@yahoo.com.br>
2023-06-23 23:55:43 +0930

    get user metrics use case

---
+++ src/use-cases/get-user-metrics.spec.ts
@@ -0,0 +1,34 @@
+import { it, describe, expect, beforeEach } from "vitest";
+
+import { InMemoryCheckInsRepository } from "@/repositories/In-memory/in-memory-checkins-repository";
+
+import { GetUserMetricsUseCase } from "./get-user-metrics";
+
+let inMemoryCheckInsRepository: InMemoryCheckInsRepository;
+
+let sut: GetUserMetricsUseCase;
+
+describe("Get User Metrics Use Case", () => {
+  beforeEach(async () => {
+    inMemoryCheckInsRepository = new InMemoryCheckInsRepository();
+    sut = new GetUserMetricsUseCase(inMemoryCheckInsRepository);
+  });
+
+  it("should be able to get check-ins count from metrics", async () => {
+    await inMemoryCheckInsRepository.create({
+      gym_id: "gym-01",
+      user_id: "user-01",
+    });
+
+    await inMemoryCheckInsRepository.create({
+      gym_id: "gym-02",
+      user_id: "user-01",
+    });
+
+    const { checkInsCount } = await sut.execute({
+      userId: "user-01",
+    });
+
+    expect(checkInsCount).toEqual(2);
+  });
+});
