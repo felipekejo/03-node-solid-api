b97e9c57ecf696899a0fca28b9b5aa21643b754e
Felipe Makoto Gushiken Yui <felipe_m87@yahoo.com.br>
2023-07-16 23:56:05 +0930

    Prisma Test Environment

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,38 @@
+import "dotenv/config";
+
+import { randomUUID } from "node:crypto";
+import { execSync } from "node:child_process";
 import { Environment } from "vitest";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not set");
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set("schema", schema);
+  return url.toString();
+}
 
 export default <Environment>{
   name: "prisma",
   async setup() {
-    console.log("Setup");
+    const schema = randomUUID();
+    const database = generateDatabaseURL(schema);
+
+    process.env.DATABASE_URL = database;
+
+    execSync("npx prisma migrate deploy");
 
     return {
       async teardown() {
-        console.log("Teardown");
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        );
+        await prisma.$disconnect();
       },
     };
   },
